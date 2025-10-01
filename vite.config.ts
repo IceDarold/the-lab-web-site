7cf78b580f33e8b9c460b6410087d1d62b54734b
Artem Konukhov <artemkonuhov008@gmail.com>
2025-10-02 01:22:10 +0300

    Small fixes

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
-    base: process.env.NODE_ENV === 'production' ? '/ai-community-landing-page/' : '/', // замените на имя вашего репозитория
+    base: process.env.NODE_ENV === 'production' ? '/the-lab-web-site/' : '/', // замените на имя вашего репозитория
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
