265b113816dc5ae7582f597c96e92fb15fecccbe
Artem Konukhov <artemkonuhov008@gmail.com>
2025-10-02 01:50:02 +0300

    Small fixed

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,4 @@
 export const config = {
-  backendUrl: 'http://the-lab-web-site-backend.vercel.app', // URL сервера бэкенда
+  backendUrl: 'https://the-lab-web-site-backend.vercel.app', // URL сервера бэкенда
   endpoint: '/api/applications' // Эндпоинт для отправки заявок
 };
